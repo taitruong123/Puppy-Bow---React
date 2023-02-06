5369f2899de616928b66672eab33f2bc75009994
Tai Truong <tylertruong2902@gmail.com>
2023-02-05 19:33:00 -0700

    add new player

--- src/searchPlayer.js
+++ src/searchPlayer.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBar = (props) => {
     return (
         <div id="search-bar">
-            <label>Search for pups by name:</label>
+            <label>Search for puppy by name:</label>
             <input name="searchTerm"></input>
             <button onClick={() =>{ props.search("")}}>Search</button>
         </div>
